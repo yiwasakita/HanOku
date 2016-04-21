fecc1a504e319f818d58ccb2c2bd2919b7d15bb6
tlsmooth89 <tlsmooth89@gmail.com>
2016-04-21 12:24:35 +0900

    First commit; before Realm implementation.

--- HanOku/ViewController.swift
+++ HanOku/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: UITableViewDataSource protocol methods
+    // Method to return the number of data/cells
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // Method to return each cell's content
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Getting reusable cells
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate protocol methods
+    // Method to be executed when a cell is selected
+    func tableView(tableView: UITableView, didselectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("cellSegue", sender: nil)
+    }
+    
+    // Method to tell that the cell is deletable
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.Delete
+    }
+    
+    // Method to be called when the Delete button is pressed
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
 }
 
