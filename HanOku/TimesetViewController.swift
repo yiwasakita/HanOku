1a3fa86133e7a05983431d18d41d3b59b0a76b32
tlsmooth89 <tlsmooth89@gmail.com>
2016-04-22 22:48:34 +0900

    The original functions all working. Can segue to /segue back from the third screen. Going to a new branch.

--- HanOku/TimesetViewController.swift
+++ HanOku/TimesetViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TimesetViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +22,4 @@ class TimesetViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
